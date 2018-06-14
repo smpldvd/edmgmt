75592ad8324d8f1c5df2cd491a6870e826c9a30b
Ivan Pawelek <ivan.pawelek@gmail.com>
2018-06-13 19:49:14 -0500

    Added on click submit function

--- assets/js/edm.js
+++ assets/js/edm.js
@@ -1,20 +1,32 @@
 var config = {
-    apiKey: "AIzaSyAL5R5ZYUrR4sRyKGHJ54HcLzp0BSJwGgI",
-    authDomain: "employee-data-management-fb0f4.firebaseapp.com",
-    databaseURL: "https://employee-data-management-fb0f4.firebaseio.com",
-    projectId: "employee-data-management-fb0f4",
-    storageBucket: "employee-data-management-fb0f4.appspot.com",
-    messagingSenderId: "263107276755"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyAL5R5ZYUrR4sRyKGHJ54HcLzp0BSJwGgI",
+  authDomain: "employee-data-management-fb0f4.firebaseapp.com",
+  databaseURL: "https://employee-data-management-fb0f4.firebaseio.com",
+  projectId: "employee-data-management-fb0f4",
+  storageBucket: "employee-data-management-fb0f4.appspot.com",
+  messagingSenderId: "263107276755"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-/*
+let name = '';
+let role = '';
+let sDate = '';
+let mRate = 0;
+
+$(document).on('click', '#submit', function (event) {
+  event.preventDefault();
+
+  name = $('#empName').val().trim();
+  role = $('#empRole').val().trim();
+  sDate = $('#empStartDate').val().trim();
+  mRate = $('#empMonthlyRate').val().trim();
+
   database.ref().push({
     name: name,
     role: role,
     sDate: sDate,
-    mRate: mRate     
+    mRate: mRate
+  });
 });
-*/
