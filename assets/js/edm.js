6b9c1f26dc47c2167b57786bff91fe1a1a99b354
David Luu <David.n.luu@gmail.com>
2018-06-13 19:54:40 -0500

    Auto stash before merge of "david" and "origin/dev"
    Fixed merge

--- assets/js/edm.js
+++ assets/js/edm.js
@@ -1,10 +1,10 @@
 var config = {
-  apiKey: "AIzaSyAL5R5ZYUrR4sRyKGHJ54HcLzp0BSJwGgI",
-  authDomain: "employee-data-management-fb0f4.firebaseapp.com",
-  databaseURL: "https://employee-data-management-fb0f4.firebaseio.com",
-  projectId: "employee-data-management-fb0f4",
-  storageBucket: "employee-data-management-fb0f4.appspot.com",
-  messagingSenderId: "263107276755"
+    apiKey: "AIzaSyAL5R5ZYUrR4sRyKGHJ54HcLzp0BSJwGgI",
+    authDomain: "employee-data-management-fb0f4.firebaseapp.com",
+    databaseURL: "https://employee-data-management-fb0f4.firebaseio.com",
+    projectId: "employee-data-management-fb0f4",
+    storageBucket: "employee-data-management-fb0f4.appspot.com",
+    messagingSenderId: "263107276755"
 };
 firebase.initializeApp(config);
 
@@ -16,17 +16,29 @@ let sDate = '';
 let mRate = 0;
 
 $(document).on('click', '#submit', function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  name = $('#empName').val().trim();
-  role = $('#empRole').val().trim();
-  sDate = $('#empStartDate').val().trim();
-  mRate = $('#empMonthlyRate').val().trim();
+    name = $('#empName').val().trim();
+    role = $('#empRole').val().trim();
+    sDate = $('#empStartDate').val().trim();
+    mRate = $('#empMonthlyRate').val().trim();
 
-  database.ref().push({
-    name: name,
-    role: role,
-    sDate: sDate,
-    mRate: mRate
-  });
+    database.ref().push({
+        name: name,
+        role: role,
+        sDate: sDate,
+        mRate: mRate
+    });
 });
+
+database.ref().on('child_added', function (childSnap) {
+    let name = childSnap.val().name;
+    let role = childSnap.val().role;
+    let sDate = childSnap.val().sDate;
+    let mRate = childSnap.val().mRate;
+
+    let $tbody = $('<tbody>');
+    let $tr = $('<tr>');
+
+    var
+})
